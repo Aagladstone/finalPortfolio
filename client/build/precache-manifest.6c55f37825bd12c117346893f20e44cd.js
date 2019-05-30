87edc0e69a7ee87198f243ae3c87bdeddbf637ad
Aaron <aarongladstone456@gmail.com>
2019-05-30 16:30:25 -0500

    fixed skills text alignment

---
+++ client/build/precache-manifest.6c55f37825bd12c117346893f20e44cd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7b7b4def9a8c11700c52cddfc5ea5b1",
+    "url": "/static/media/trains.d7b7b4de.PNG"
+  },
+  {
+    "revision": "79dcd130f57a8e2e5030",
+    "url": "/static/css/main.6816ab98.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9923a53c4122b9f40ae",
+    "url": "/static/js/2.18446ec5.chunk.js"
+  },
+  {
+    "revision": "03d98a72ca292221e6fdaf2907ea660c",
+    "url": "/static/media/headshot.03d98a72.PNG"
+  },
+  {
+    "revision": "b6ab84ea34bf737051dd6d2df1ea4880",
+    "url": "/static/media/trackerLogin.b6ab84ea.PNG"
+  },
+  {
+    "revision": "091e8c2bc98b90e063c1e94a7368a87f",
+    "url": "/static/media/trackerUserpage.091e8c2b.PNG"
+  },
+  {
+    "revision": "2c44b1379df3b3c9b91f20a01017c84e",
+    "url": "/static/media/datebookOne.2c44b137.PNG"
+  },
+  {
+    "revision": "9332773e6902851c69768d7af3cf6e17",
+    "url": "/static/media/datebookTwo.9332773e.PNG"
+  },
+  {
+    "revision": "18c116cc3b79c59babb7de90b32a4fb4",
+    "url": "/static/media/callingMeOne.18c116cc.PNG"
+  },
+  {
+    "revision": "996edfdfe2ce53fbfd89a11de16b7895",
+    "url": "/static/media/callingMeTwo.996edfdf.PNG"
+  },
+  {
+    "revision": "79dcd130f57a8e2e5030",
+    "url": "/static/js/main.37899370.chunk.js"
+  },
+  {
+    "revision": "bff730239e9d8706f42dd3a01dfcb420",
+    "url": "/static/media/newTrain.bff73023.PNG"
+  },
+  {
+    "revision": "9e994545748851e6457c2e87376d0c32",
+    "url": "/static/media/dakgif.9e994545.PNG"
+  },
+  {
+    "revision": "0fa0a0ddd345ce856c848c6cc9f964a1",
+    "url": "/static/media/toddgif.0fa0a0dd.PNG"
+  },
+  {
+    "revision": "2a670acba127beed1d1908250b051e57",
+    "url": "/static/media/Spotify-This-Song-Complete.2a670acb.png"
+  },
+  {
+    "revision": "2357dcaeb66508343dfb7f1c6893fba0",
+    "url": "/static/media/Do-what-it-says-movie.2357dcae.png"
+  },
+  {
+    "revision": "2e940b0e720f42fb512437f18e448660",
+    "url": "/static/media/customer-start.2e940b0e.PNG"
+  },
+  {
+    "revision": "6817f5e618c9953c6ca607c9be65a7c8",
+    "url": "/static/media/manager-order-more.6817f5e6.PNG"
+  },
+  {
+    "revision": "311f3e9efd02400ea7fbee6dad9213e7",
+    "url": "/static/media/burger.311f3e9e.PNG"
+  },
+  {
+    "revision": "79f8916a9442efda9ec26d3f1c16df50",
+    "url": "/static/media/eatenBurger.79f8916a.PNG"
+  },
+  {
+    "revision": "be5866d26fa1e6a9a3e414062d3f7af0",
+    "url": "/index.html"
+  }
+];
