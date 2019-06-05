32b9b8be996484e09345ffed50e5288ccf33b8f0
Aaron <aarongladstone456@gmail.com>
2019-06-05 09:27:36 -0500

    clean up

--- client/src/Pages/Home/Projects.js
+++ client/src/Pages/Home/Projects.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
-// import Navbar from '../../components/Navbar/Navbar'
 import Wrapper from "../../components/Wrapper/Wrapper";
 import ContentBox from "../../components/Content Box/Contentbox"
-import Github, { SocialsBox, WorkSocials, Twitter, Instagram, Resume } from "../../components/Socials/socials";
-import {Image, Headshot} from '../../components/Images/Image'
 import {ProjectThree, ProjectTwo, ProjectOne} from '../../components/Projects/Projects'
 import "./style.css";
 
